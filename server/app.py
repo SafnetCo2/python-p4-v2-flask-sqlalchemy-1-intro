7537e1ebdae51060aa3cee4d413555c084a469a1
Jospine <nzi19001@byui.edu>
2024-06-06 12:43:19 +0300

    done

--- server/app.py
+++ server/app.py
@@ -1,25 +1,35 @@
-# server/app.py
-
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
-from models import db
-
-# create a Flask application instance
+# Create a Flask application instance
 app = Flask(__name__)
 
-# configure the database connection to the local file app.db
+# Configure the database connection to the local file app.db
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 
-# configure flag to disable modification tracking and use less memory
+# Configure flag to disable modification tracking and use less memory
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# create a Migrate object to manage schema modifications
+# Create a SQLAlchemy object to represent the database
+db = SQLAlchemy(app)
+
+# Create a Migrate object to manage schema modifications
 migrate = Migrate(app, db)
 
-# initialize the Flask application to use the database
-db.init_app(app)
+# Define models
+class Pet(db.Model):
+    __tablename__ = 'pets'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    species = db.Column(db.String)
 
+class User(db.Model):
+    __tablename__ = 'users'  # Changed table name to 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String)
+    species = db.Column(db.String)
+    verified = db.Column(db.Boolean, default=False)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
